be726abbea304eb77c7ac8a56b43a092e359e8f8
MdoubleA <32501929+MdoubleA@users.noreply.github.com>
2019-11-21 00:02:06 -0800

    Initial commit

---
+++ Client.py
@@ -0,0 +1,142 @@
+from twisted.internet.protocol import Protocol
+#from twisted.protocols import basic
+from sys import stdout
+from twisted.internet import reactor
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+from twisted.application.internet import ClientService
+from twisted.internet import stdio
+
+
+serverIP = "10.0.0.58" # Stevens ip address #"10.200.185.199"  # school ip address  #"192.168.0.19" # Bob's ip  #"10.0.0.58" #Home ip address
+serverPort = 8007
+
+
+def buffered_payload_len(payload, offset):
+    buffer = offset - len(str(len(payload)))
+    return "0"*buffer + str(len(payload))
+
+class Client(Protocol):
+    def __init__(self):
+        self.phase = "GREET"
+        self.user_name = None
+
+    def connectionMade(self):
+        data = b"HELLO"
+        self.transport.write(data)
+
+    def dataReceived(self, data):
+        if self.phase == "GREET":
+            self.handle_GREET(data)
+
+        elif self.phase == "CONVO":
+            self.handle_CONVO(data)
+
+    def handle_GREET(self, data):
+        switcher = {
+            'WHAT!': self.get_credentials,
+            'NACK!': self.get_credentials,  # time out mechanism
+            'ACK!!': self.phase_transition,
+        }
+
+        switcher.get(data[:5].decode(), self.panic)()
+
+    def handle_CONVO(self, data):
+        switcher = {
+            'LIST!': self.catch_list,
+            'CREAT': self.catch_create,
+            'JOIN!': self.catch_join,
+            'MESS!': self.catch_message,
+        }
+
+        switcher.get(data[:5].decode(), self.panic)(data[5:])
+
+    def get_credentials(self):
+        self.user_name = input("Type user name: ")  # APPLICATION
+        data = "MAKE!" + self.user_name
+        self.transport.write(data.encode())
+
+    def converse(self):
+        self.transport.write(self.menu())
+
+    def list(self, data):
+        self.transport.write(data.encode())
+
+    def phase_transition(self):
+        self.phase = "CONVO"
+        self.converse()
+
+    '''
+    Need to double check that we validate input.
+    '''
+    def menu(self):
+        menu = input("\nList 0\nCreate 1\nJoin Room 2\n")
+
+        if menu[0] == '0':
+            menu = "LIST!"
+        elif menu[0] == '1':
+            menu = "CREAT" + buffered_payload_len(self.user_name, 8) + self.user_name + input("Enter Room Name: ")
+        elif menu[0] == '2':
+            menu = "JOIN!" + buffered_payload_len(self.user_name, 8) + self.user_name + input("Enter Room Name: ")
+
+        return menu.encode()
+
+    def catch_list(self, data):
+        message_length = int(data[0:8].decode())  # 5 byte header 8 byte message length
+        if message_length == 0:
+            stdout.write("No chat rooms")
+        else:
+            message = data[8:].decode()
+            stdout.write(message)
+        self.converse()
+
+    def catch_join(self, data):
+        if data[:5].decode() == "NACK!":
+            stdout.write("No, the room does not exist.\n")
+            self.converse()
+        else:
+            room_name = data[5:].decode()
+            stdout.write("Welcome " + self.user_name + " to " + room_name + ".\n")  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            mssg = input("Enter Message: ")
+
+            if mssg == "REMOVE ME FROM THE ROOM!!!":
+                stdout.write("Girl bye.")  # !!!!!!!!!!!!!!!!!!!!
+                self.converse()
+                return
+
+            self.room_name = room_name
+            mssg = "MESS!" + buffered_payload_len(room_name, 8) + room_name + self.user_name + ": " + mssg
+            self.transport.write(mssg.encode())
+
+    def catch_message(self, data):  # Assume one room at a time.
+        stdout.write(data.decode() + "\n")
+
+        mssg = input("Enter Message: ")
+        if mssg == "REMOVE ME FROM THE ROOM!!!":
+            stdout.write("Girl bye.")
+            self.converse()
+            return
+        mssg = "MESS!" + buffered_payload_len(self.room_name, 8) + self.room_name + self.user_name + ": " + mssg
+        self.transport.write(mssg.encode())
+
+    def catch_create(self, data):
+        mssg = {"NACK": "No. Try again.\n", "ACK": "We've made your room.\n"}
+        stdout.write(mssg[data.decode()])
+        self.converse()
+
+    def panic(self):
+        stdout.write("PANIC!!!!\n")
+
+def end():
+    stdout.write("turning off\n")
+    reactor.stop()
+
+
+socket = TCP4ClientEndpoint(reactor, serverIP, serverPort)
+proto = connectProtocol(socket, Client())
+theConnection = ClientService(socket, proto)
+
+theConnection.startService()
+
+#reactor.callLater(20, end)
+reactor.run()  # Begin running Twisted's OS interacting processes.
+
---
+++ Server.py
@@ -0,0 +1,135 @@
+from twisted.internet.protocol import Protocol
+from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet import reactor
+from twisted.internet.protocol import Factory
+from sys import stdout
+
+
+def buffered_payload_len(payload, offset):
+    buffer = offset - len(str(len(payload)))
+    return "0"*buffer + str(len(payload))
+
+
+# This is the protocol that translates the applications messages
+class Chat(Protocol):
+    def __init__(self, client_list, chatroom_list):
+        self.client_list = client_list
+        self.chatroom_list = chatroom_list
+        self.user_name = None
+        self.phase = "GREET"
+
+    def connectionLost(self, reason):  # make original
+        if self.user_name in self.client_list:
+            del self.client_list[self.user_name]
+
+    def dataReceived(self, data):
+        if self.phase == "GREET":
+            self.handle_GREET(data)
+
+        elif self.phase == "CONVO":
+            self.handle_CONVO(data)
+
+    def handle_GREET(self, data):
+        switcher = {
+            "HELLO": self.hello,
+            "MAKE!": self.make,
+        }
+
+        switcher.get(data[:5].decode(), self.panic)(data[5:])
+
+    def handle_CONVO(self, data):
+        switcher = {
+            "LIST!": self.list,
+            "CREAT": self.create,
+            "JOIN!": self.join,
+            "MESS!": self.message,
+        }
+
+        switcher.get(data[:5].decode(), self.panic)(data[5:])
+
+    def get_user_room_names(self, data):  # pass bytes get string
+        user_name_length = int(data[:8].decode())
+        user_name = data[8:user_name_length].decode()
+        room_name = data[8+user_name_length:].decode()
+        return user_name, room_name
+
+    def message(self, data):
+        name_len = int(data[:8].decode())
+        room_name = data[8:8+name_len].decode()
+        message = data[8+name_len:]
+
+        for aChat in self.chatroom_list[room_name]:
+            if aChat.user_name != self.user_name:
+                aChat.transport.write(b"MESS!" + message)
+
+    def join(self, data):
+        name_len = int(data[:8].decode())
+        user_name = data[8:8+name_len].decode()
+        room_name = data[8+name_len:].decode()
+
+        if room_name not in self.chatroom_list:  # duplicate chat room names
+            self.transport.write(b"JOIN!NACK!")
+            return
+
+        a = "JOIN!ACK!!" + room_name
+
+        self.chatroom_list[room_name].append(self.client_list[user_name])
+        self.transport.write(a.encode())
+
+    def create(self, data):
+        name_len = int(data[:8].decode())
+        user_name = data[8:8+name_len].decode()
+        room_name = data[8+name_len:].decode()
+
+        if room_name in self.chatroom_list:
+            self.transport.write(b"CREATNACK")
+            return
+
+        self.chatroom_list[room_name] = list()
+        self.transport.write(b"CREATACK")
+
+    def list(self, data):
+        if self.chatroom_list:
+            payload = '\n'.join(list(self.chatroom_list.keys()))+'\n'
+        else:
+            payload = ''
+
+        payload = "LIST!" + buffered_payload_len(payload, 8) + payload
+        self.transport.write(payload.encode())
+
+    def hello(self, data):  # data == HELLO
+        data = b'WHAT!'
+        self.transport.write(data)
+
+    def make(self, name):
+        # If add password, parse byte lengths of username and password
+        name = name.decode()
+        if name in self.client_list:
+            self.transport.write(b'NACK!')
+        else:
+            self.user_name = name
+            self.client_list[self.user_name] = self
+            self.phase = "CONVO"
+            self.transport.write(b'ACK!!')
+
+    def panic(self):
+        stdout.write("PANIC!!!!")
+        reactor.stop()
+
+
+class ChatFactory(Factory):
+    def __init__(self):
+        self.client_list = {} # maps user names to Chat instances
+        self.chatroom_list = {}
+
+    def buildProtocol(self, addr):
+        return Chat(self.client_list, self.chatroom_list)
+
+
+endpoint = TCP4ServerEndpoint(reactor, 8007)  # A TCP socket
+endpoint.listen(ChatFactory())
+reactor.run()
+
+
+
+
